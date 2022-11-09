7302dbf4ab99d154704151ac1e52f5603b872fc0
Regina Dulaeva <dulaeva_regina@mail.ru>
2022-11-09 23:10:41 +0300

    Lesson 3v1

---
+++ Lesson 3/Exercise 2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+
+int Prompt(string massage)
+{
+    Console.Write(massage);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+double x1 = Prompt("Введите координаты первой точки по оси x: ");
+double y1 = Prompt("Введите координаты первой точки по оси y: ");
+double z1 = Prompt("Введите координаты первой точки по оси z: ");
+double x2 = Prompt("Введите координаты второй точки по оси x: ");
+double y2 = Prompt("Введите координаты второй точки по оси y: ");
+double z2 = Prompt("Введите координаты второй точки по оси z: ");
+double Distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+Console. WriteLine($"Расстояние между двумя точками равно: {Distance}");
