dbcc062760ebdd690be218b2ef55ca9cab5df898
Regina Dulaeva <dulaeva_regina@mail.ru>
2022-11-09 23:27:39 +0300

    all changes

---
+++ Lesson 3v1/Exercise 3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+int Prompt(string massage)
+{
+    Console.Write(massage);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+bool ValidateNumber(int number)
+{
+    if(number < 1)
+    {
+        Console.WriteLine("Необходимо ввести число > 1");
+        return false;
+    }
+    return true;
+}
+int number = Prompt("Введите число: ");
+if(ValidateNumber(number))
+{
+        for(int i = 1; i <= number; i++)
+       {
+       int stepen = 3;
+       Console.WriteLine($"{i}^{stepen} = {Math.Pow(i,stepen)}");
+       }
+       
+    }
+
